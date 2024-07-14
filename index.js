d6a1a6ff1a11c55725ac66c4b71082f1a2a69398
NI8IRU <francesco.tornambe@gmail.com>
2024-07-14 14:04:58 +0200

    Aggiornamento comportamento Nome durante la navigazione sulla navbar

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const startTime = new Date()
+
+function getTema() {
+    // // Ottengo il tema della pagina
+    let tema
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        tema = "dark"
+    } else {
+        tema = "light"
+    }
+
+    return tema
+}
+
+function measureSecPassed() {
+    const endTime = new Date()
+    let timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff /= 1000
+  
+    // get seconds 
+    let seconds = Math.round(timeDiff)
+    return seconds
+  }
+
+function navbarSvgWorkFlow(direction) {
+    let svgName = document.getElementById("name")
+
+    //Imposto un animazione infinità se il mouse e sopra la navbar
+    //Il testo viene resettato se il mouse si sposta fuori
+    if (measureSecPassed() > 4) {
+        if (direction === "in") {
+            svgName.style.animation = "name-work-flow 3s cubic-bezier(0.1, 0.2, 0.1, 0.3) infinite"
+            svgName.style.stroke = getTema() === "light" ? "black" : "#E0E1DD"
+            svgName.style.strokeDasharray = "80"
+        } else if (direction === "out") {
+            const fadeOut = getTema() === "light" ? "fade-out-light 1.5s forwards" : "fade-out-dark 1.5s forwards"
+            svgName.style.animation = "name-work-flow 3s cubic-bezier(0.1, 0.2, 0.1, 0.3) infinite, " + fadeOut
+            setTimeout(() => {
+                svgName.style.animation = null
+                svgName.style.strokeDasharray = null
+            }, 1500);
+        }
+    }
+}
+
 function hamburgerMenuWorkFlow(action) {
 
     switch (action) {
