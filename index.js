f0b8030cf9010df0f03272b5439b037826afc26f
NI8IRU <francesco.tornambe@gmail.com>
2024-08-03 09:33:48 +0200

    Pulizia codice

--- index.js
+++ index.js
@@ -83,16 +83,15 @@ function measureSecPassed(startTime, endTime = new Date()) {
 function hamburgerMenuWorkFlow(action) {
 
     switch (action) {
-        case "click":
-            hamburgerMenuHoverClick()
-            break;
-
         case "in":
             hamburgerMenuHoverIn()
             break;
 
         case "out":
             hamburgerMenuHoverOut()
+            break;
+
+        default:
             break;
     }
 }
@@ -143,47 +142,4 @@ function hamburgerMenuHoverOut() {
             hamburgerMenu.style.color = "#E0E1DD"
             break;
     }
-}
-
-function hamburgerMenuHoverClick() {
-
-    // // Ottengo il tema della pagina
-    // let tema
-    // if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    //     tema = "dark"
-    // } else {
-    //     tema = "light"
-    // }
-
-    // let btn = document.getElementById("hamburgerMenuBtn")
-    // let hamburgerMenu = document.getElementById("hamburgerMenu")
-    // let isAreaExpanded = btn.getAttribute("aria-expanded")
-    // console.log(isAreaExpanded)
-
-    // //Cambio il colore in base al tema
-    // switch (tema) {
-    //     case "light":
-    //         if (isAreaExpanded) {
-    //             hamburgerMenu.style.color = "#E0E1DD";
-    //             btn.style.backgroundColor = "#181B19";
-    //         } else {
-    //             hamburgerMenu.style.color = "#181B19";
-    //             btn.style.backgroundColor = "#E0E1DD";
-    //         }
-    //         break;
-
-    //     case "dark":
-    //         if (isAreaExpanded) {
-    //             hamburgerMenu.style.color = "#181B19";
-    //             btn.style.backgroundColor = "#E0E1DD";
-    //         } else {
-    //             hamburgerMenu.style.color = "#E0E1DD";
-    //             btn.style.backgroundColor = "#181B19";
-    //         }
-    //         break;
-
-    //     default:
-    //         console.error(`Tema sconosciuto: ${tema}`);
-    //         break;
-    // }
 }
